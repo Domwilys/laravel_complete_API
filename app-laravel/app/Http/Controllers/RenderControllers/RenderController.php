409e68fb5816d360b1edc032323c5f5a9f03d062
Domwillys <willyanpaproski123@gmail.com>
2023-09-03 16:42:19 -0300

    migrations

---
+++ app-laravel/app/Http/Controllers/RenderControllers/RenderController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\RenderControllers;
+
+class RenderController
+{
+    public function indexRender() {
+        return view('index');
+    }
+}
--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\RenderControllers\RenderController;
+use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SiteController::class, 'index']);
+Route::get('/', [RenderController::class, 'indexRender']);
+Route::get('/supports', [SupportController::class, 'index']) -> name('supports.index');
