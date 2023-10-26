5441acdd6420af67bd16f75477ae4e102af32589
Domwillys <willyanpaproski123@gmail.com>
2023-10-26 14:27:40 -0300

    need some visual fixes

--- app-laravel/resources/views/dashboard.blade.php
+++ app-laravel/resources/views/dashboard.blade.php
@@ -18,7 +18,26 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     Acessar painel de dados dos Depoimentos
-                    <a href="{{ url('/depoiments') }}" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    <a href="{{ url('/depoiments') }}" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white">
+                        <span class="svg-icon svg-icon-primary svg-icon-2x">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="-mb-2">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" class="-mb-8">
+                                    <polygon points="0 0 24 0 24 24 0 24"/>
+                                    <rect fill="#9ca3af" transform="translate(8.500000, 12.000000) rotate(-90.000000) translate(-8.500000, -12.000000) " x="7.5" y="7.5" width="2" height="9" rx="1"/>
+                                    <path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#9ca3af" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997) "/>
+                                </g>
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-3">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    Cadastrar um novo Depoimento
+                    <a href="{{ url('/depoiments/create') }}" class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white">
                         <span class="svg-icon svg-icon-primary svg-icon-2x">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="-mb-2">
                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" class="-mb-8">
--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -16,20 +16,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-//     Route::delete('/supports/{id}', [SupportController::class, 'delete']) -> name('supports.delete');
-//     Route::put('/supports/{id}', [SupportController::class, 'update']) -> name('supports.update');
-//     Route::get('/supports/{id}/edit', [SupportController::class, 'edit']) -> name('supports.edit');
-//     Route::get('/supports', [SupportController::class, 'index']) -> name('supports.index');
-//     Route::get('/supports/create', [SupportController::class, 'create']) -> name('supports.create');
-//     Route::post('/supports/create', [SupportController::class, 'store']) -> name('supports.store');
-//     Route::get('supports/{id}', [SupportController::class, 'show']) -> name('supports.show');
-// });
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
